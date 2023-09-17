d1fc8d893bdd1777d1ee9437452b7929bb0793fa
Ammar.Shaker <Ammar.Shaker@PBDAC.LOCAL>
2023-09-17 14:21:31 +0300

    updateed

--- Training/src/test/java/procedures/OpenIslamicSavingsAccountProcedures.java
+++ Training/src/test/java/procedures/OpenIslamicSavingsAccountProcedures.java
@@ -9,13 +9,8 @@ public class OpenIslamicSavingsAccountProcedures {
 		OpenIslamicSavingsAccountPage OpenIslamicSavingsAccountPage = new OpenIslamicSavingsAccountPage(driver);
 		OpenIslamicSavingsAccountPage.sendKeysSearchBarTextField(data.getMenu())
 									  .switchFormAreaFrame()
-<<<<<<< HEAD
-									   .addAccountDetails(data.getCif())
+									   .sendKeysAccoundID(data.getCif())
 									    .pressSubmitButton()
 										 .saveAccountId();
-=======
-									   .sendKeysAccoundID(data.getCif())
-									    .pressSubmitButton();
->>>>>>> 14e52afa61a4905f2b94932e32f35455606af542
 		}
 	}

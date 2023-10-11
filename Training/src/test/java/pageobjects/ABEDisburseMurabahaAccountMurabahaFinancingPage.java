e72010d42aea543b5f9bc95cf2c8a9b50dd2f885
Ammar.Shaker <Ammar.Shaker@PBDAC.LOCAL>
2023-10-11 15:41:42 +0300

    warnings removed

--- Training/src/test/java/pageobjects/ABEDisburseMurabahaAccountMurabahaFinancingPage.java
+++ Training/src/test/java/pageobjects/ABEDisburseMurabahaAccountMurabahaFinancingPage.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import io.qameta.allure.Step;
 import utils.CSVUtils;

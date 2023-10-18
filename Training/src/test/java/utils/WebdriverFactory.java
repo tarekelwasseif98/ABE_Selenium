b1a7ae0af35434906bc36ed28bddb782182b0208
Ammar.Shaker <Ammar.Shaker@PBDAC.LOCAL>
2023-10-18 12:48:31 +0300

    done

--- Training/src/test/java/utils/WebdriverFactory.java
+++ Training/src/test/java/utils/WebdriverFactory.java
@@ -10,7 +10,7 @@ import java.net.URL;
 
 	public class WebdriverFactory {
 		public static WebDriver initiateWebDriver() throws MalformedURLException {
-			DesiredCapabilities cap=new DesiredCapabilities();
+			DesiredCapabilities cap = new DesiredCapabilities();
 			cap.setBrowserName("chrome");
 			cap.setPlatform(Platform.WINDOWS);
 			ChromeOptions options = new ChromeOptions();

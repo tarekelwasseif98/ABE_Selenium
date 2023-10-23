36111f77de0b34e7dcdd596bf582ff10b4c873d4
tarekelwasseif98 <44944956+tarekelwasseif98@users.noreply.github.com>
2023-10-23 13:54:16 +0300

    Delete ABE_Automation_Project directory

--- ABE_Automation_Project/src/test/java/executionlist/execute.bat
+++
@@ -1,4 +0,0 @@
-set projectLocation=C:\Users\Mostafa.Abdeltawab\eclipse-workspace\Training
-cd C:\Users\Mostafa.Abdeltawab\eclipse-workspace\Training
-set classpath=C:\Users\Mostafa.Abdeltawab\eclipse-workspace\Training\bin;C:\Users\Mostafa.Abdeltawab\eclipse-workspace\Training\libs\*;C:\Users\Mostafa.Abdeltawab\.m2\repository\org\testng\testng\7.8.0\
-java org.testng.testng C:\Users\Mostafa.Abdeltawab\eclipse-workspace\Training\src\test\java\executionlist\ExecutionList1.xml

489c7576b588ab17a300dc1f05d28ac5e41626e3
Ammar.Shaker <Ammar.Shaker@PBDAC.LOCAL>
2023-11-19 15:12:34 +0200

    tarek scripts

--- Training/src/test/java/utils/Paths.java
+++ Training/src/test/java/utils/Paths.java
@@ -60,29 +60,4 @@ public class Paths {
 	public static final String ABEVERIFYREALIZEEXPORTBILLSTRADEFINANCECSV  = "csvFiles\\ABEVerifyRealizeExportBillsTradeFinance.csv";
 	public static final String ABEREALIZEEXPORTBILLSTRADEFINANCEJSON =  "jsonFiles\\" + ABEREALIZEEXPORTBILLSTRADEFINANCECSV.substring(9, Paths.ABEREALIZEEXPORTBILLSTRADEFINANCECSV.length()-4) + ".json";
 	public static final String ABEVERIFYREALIZEEXPORTBILLSTRADEFINANCEJSON =  "jsonFiles\\" + ABEVERIFYREALIZEEXPORTBILLSTRADEFINANCECSV.substring(9, Paths.ABEVERIFYREALIZEEXPORTBILLSTRADEFINANCECSV.length()-4) + ".json";
-	
-	public static final String Create_ACH_OutwardCsv  = "csvFiles\\Create_ACH_Outward.csv";
-	public static final String Verify_ACH_Outwardcsv = "csvFiles\\Verify_ACH_Outward.csv";
-	public static final String Authorize_ACH_Outwardcsv = "csvFiles\\Authorize_ACH_Outward.csv";
-	public static final String Create_ACH_OutwardJson =  "jsonFiles\\" + Create_ACH_OutwardCsv.substring(9, Paths.Create_ACH_OutwardCsv.length()-4) + ".json";
-	public static final String Verify_ACH_OutwardJson = "jsonFiles\\" + Verify_ACH_Outwardcsv.substring(9, Paths.Verify_ACH_Outwardcsv.length()-4) + ".json";
-	public static final String Authorize_ACH_OutwardJson =  "jsonFiles\\" + Authorize_ACH_Outwardcsv.substring(9, Paths.Authorize_ACH_Outwardcsv.length()-4) + ".json";
-	
-	public static final String ABECreateCASACurrentAccountLiabilitiesAndOperationCsv  = "csvFiles\\ABECreateCASACurrentAccountLiabilitiesAndOperation.csv";
-	public static final String ABECreateCASACurrentAccountLiabilitiesAndOperationJson = "jsonFiles\\" + ABECreateCASACurrentAccountLiabilitiesAndOperationCsv.substring(9, Paths.ABECreateCASACurrentAccountLiabilitiesAndOperationCsv.length()-4) + ".json";
-	public static final String ABEVerifyCasaCurrentAccountCreationLiabilitiesAndOperationCsv  = "csvFiles\\ABEVerifyCasaCurrentAccountCreationLiabilitiesAndOperation.csv";
-	public static final String ABEVerifyCasaCurrentAccountCreationLiabilitiesAndOperationJson = "jsonFiles\\" + ABEVerifyCasaCurrentAccountCreationLiabilitiesAndOperationCsv.substring(9, Paths.ABEVerifyCasaCurrentAccountCreationLiabilitiesAndOperationCsv.length()-4) + ".json";
-
-	public static final String ABECreateCASASavingsAccountLiabilitiesAndOperationCsv  = "csvFiles\\ABECreateCASASavingsAccountLiabilitiesAndOperation.csv";
-	public static final String ABECreateCASASavingsAccountLiabilitiesAndOperationJson = "jsonFiles\\" + ABECreateCASASavingsAccountLiabilitiesAndOperationCsv.substring(9, Paths.ABECreateCASASavingsAccountLiabilitiesAndOperationCsv.length()-4) + ".json";
-	public static final String ABEVerifyCASASavingsAccountCreationLiabilitiesAndOperationCsv  = "csvFiles\\ABEVerifyCASASavingsAccountCreationLiabilitiesAndOperation.csv";
-	public static final String ABEVerifyCASASavingsAccountCreationLiabilitiesAndOperationJson = "jsonFiles\\" + ABEVerifyCASASavingsAccountCreationLiabilitiesAndOperationCsv.substring(9, Paths.ABEVerifyCASASavingsAccountCreationLiabilitiesAndOperationCsv.length()-4) + ".json";
-	
-	public static final String ABECreateOfficeAccountFinanceCsv  = "csvFiles\\ABECreateOfficeAccountFinance.csv";
-	public static final String ABEVerifyOfficeAccountFinanceCsv  = "csvFiles\\ABEVerifyOfficeAccountFinance.csv";
-	public static final String ABECreateOfficeAccountFinanceJson =  "jsonFiles\\" + ABECreateOfficeAccountFinanceCsv.substring(9, Paths.ABECreateOfficeAccountFinanceCsv.length()-4) + ".json";
-	public static final String ABEVerifyOfficeAccountFinanceJson =  "jsonFiles\\" + ABEVerifyOfficeAccountFinanceCsv.substring(9, Paths.ABEVerifyOfficeAccountFinanceCsv.length()-4) + ".json";
-	
-	public static final String ABETellerOperationTransferLiabilitiesAndOperationCsv  = "csvFiles\\ABETellerOperationTransferLiabilitiesAndOperation.csv";
-	public static final String ABETellerOperationTransferLiabilitiesAndOperationJson =  "jsonFiles\\" + ABETellerOperationTransferLiabilitiesAndOperationCsv.substring(9, Paths.ABETellerOperationTransferLiabilitiesAndOperationCsv.length()-4) + ".json";
 }
